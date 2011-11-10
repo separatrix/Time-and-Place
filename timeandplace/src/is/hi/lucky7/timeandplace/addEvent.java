6c695a0721c191b7a47fca4532b330e8eb34b881
imbakassi <imbakassi@gmail.com>
2011-11-10 18:47:24 +0000

    Baetti vid minu doti

--- timeandplace/src/is/hi/lucky7/timeandplace/addEvent.java
+++ timeandplace/src/is/hi/lucky7/timeandplace/addEvent.java
@@ -1,16 +1,94 @@
 package is.hi.lucky7.timeandplace;
 
+import java.util.Calendar;
+
 import android.os.Bundle;
-import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
 import android.app.Activity;
-import android.content.Intent;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.content.DialogInterface;
 import android.view.View;
 import is.hi.lucky7.timeandplace.Event;
 import is.hi.lucky7.timeandplace.DBAdapter;
 
 public class addEvent extends Activity{
 	private DBAdapter dbAdapter;
+    /** Called when the activity is first created. */
+    private int ar;
+    private int man;
+    private int dagur;
+    private int klst;
+    private int min;
+    //Tími upphafs atburðar
+    private int f_klst;
+    private int f_min;
+    //Tími loka atburðar
+    private int s_klst;
+    private int s_min;
+    private int ferdamati;
+    
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+        //Dagsetningar dialog
+        case 0:
+            return new DatePickerDialog(this,
+                        veljadag,
+                        ar, man, dagur);
+        //Fyrra tíma dialog
+        case 1:
+            return new TimePickerDialog(this,
+                    mTimeSetListener, klst, min, false);
+        //Seinna tíma dialog
+    	case 2: 
+    		return new TimePickerDialog(this,
+                mTimeSetListener2, f_klst, f_min, false);
+    	//Ferðamáta dialog
+    	case 3:
+    		String num[] = {"Gangandi","Hjólandi","Keyrandi"}; 
+    		return new AlertDialog.Builder(addEvent.this)
+            	.setTitle("Ferðamáti")
+            	.setItems(num, new DialogInterface.OnClickListener() {
+            		public void onClick(DialogInterface dialog, int which) {
+            			//0 = gangandi, 1 = hjólandi, 2 = keyrandi
+            			ferdamati = which;
+            			}
+            })
+            .create();
+        }
+        return null;
+    }
+    //Dagsetning
+    private DatePickerDialog.OnDateSetListener veljadag =
+            new DatePickerDialog.OnDateSetListener() {
+                public void onDateSet(DatePicker view, int year, 
+                                      int monthOfYear, int dayOfMonth) {
+                    ar = year;
+                    man = monthOfYear;
+                    dagur = dayOfMonth;
+                }
+            };
+    //Fyrrra skiptið sem tíminn birtist
+    private TimePickerDialog.OnTimeSetListener mTimeSetListener =
+            new TimePickerDialog.OnTimeSetListener() {
+                public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                	f_klst = hourOfDay;
+                	f_min = minute;
+                	//showDialog(2);
+                }
+            };
+    //Seinna skiptið sem tíminn birtist
+    private TimePickerDialog.OnTimeSetListener mTimeSetListener2 =
+            new TimePickerDialog.OnTimeSetListener() {
+                public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                    s_klst = hourOfDay;
+                    s_min = minute;
+                        }
+                    };
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,37 +97,48 @@ public class addEvent extends Activity{
         dbAdapter = new DBAdapter(this);
         dbAdapter.open();
         
-		final EditText edit_nafn = (EditText) findViewById(R.id.edi_nafn);
-		final EditText edit_startTime = (EditText) findViewById(R.id.edi_timasetning);
-		//final EditText edit_endTime = (EditText) findViewById(R.id.editText_endtime);
-		final EditText edit_location = (EditText) findViewById(R.id.edi_stadsetning);
-		//final EditText edit_info = (EditText) findViewById(R.id.editText_info);
-
-        final Button addEventButton = (Button) findViewById(R.id.btn_skra);
+        // Núverandi dagsetning
+        final Calendar c = Calendar.getInstance();
+        ar = c.get(Calendar.YEAR);
+        man = c.get(Calendar.MONTH);
+        dagur = c.get(Calendar.DAY_OF_MONTH);
+        klst = c.get(Calendar.HOUR_OF_DAY);
+        min = c.get(Calendar.MINUTE);
 
 	}
-        public void BaetaVid(View view) {
-    		final EditText edit_nafn = (EditText) findViewById(R.id.edi_nafn);
-    		final EditText edit_startTime = (EditText) findViewById(R.id.edi_timasetning);
-    		//final EditText edit_endTime = (EditText) findViewById(R.id.editText_endtime);
-    		final EditText edit_location = (EditText) findViewById(R.id.edi_stadsetning);
-    		//final EditText edit_info = (EditText) findViewById(R.id.editText_info);
-        	String name = edit_nafn.getText().toString();
-    		int start = Integer.parseInt(edit_startTime.getText().toString());
-    		//int end = Integer.parseInt(edit_endTime.getText().toString());
-    		int end = 0;
-    		String loc = edit_location.getText().toString();
-    		//String info = edit_info.getText().toString();
-    		String info = "NULL";
-    		
-    		// Herna vantar villutjekk. Forritid crashar ef null gildi eru skrad inn i event
-    		// thar sem dalkar in gagnagrunn eru restricted vid NOT NULL gildi.
-    		// Tharf ad passa ad name, start_time, end_time og transport seu ekki null.
-    		// Spurning um ad passa lika upp a location upp a seinni tima.
-    		
-    		// Ath einnig nyjan Event smid sem tekur lika vid transport
-    		Event e = new Event(name, start, end, loc, info);
-    		dbAdapter.AddEvent(e);
+    public void fSkra(View view) {
+    	//Hér þarf líka að bakka á upphafsskjá.
+    	final EditText edit_nafn = (EditText) findViewById(R.id.edi_nafn);
+    	final EditText edit_lysing = (EditText) findViewById(R.id.edi_lysing);
+        String nafn = edit_nafn.getText().toString();
+    	String lysing = edit_lysing.getText().toString();
+    	int fyrritimi = toTimestamp(ar,man,dagur,f_klst,f_min);
+    	int seinnitimi = toTimestamp(ar,man,dagur,s_klst,s_min);
+    	String stadsetning = "NULL";
+    	// Herna vantar villutjekk. Forritid crashar ef null gildi eru skrad inn i event
+    	// thar sem dalkar in gagnagrunn eru restricted vid NOT NULL gildi.
+    	// Tharf ad passa ad name, start_time, end_time og transport seu ekki null.
+    	// Spurning um ad passa lika upp a location upp a seinni tima.	
+    	Event e = new Event(nafn,fyrritimi,seinnitimi,stadsetning,ferdamati,lysing);
+    	dbAdapter.AddEvent(e); 
+    	}
+    public void fDag(View view) {
+    	showDialog(0);
+    	    }
+    public void fUpphaf(View view) {
+    	showDialog(1);
+    	    }
+    public void fLoka(View view) {
+    	showDialog(2);
+    	    }
+    public void fFerda(View view) {
+    	showDialog(3);
+    	    }
+    public int toTimestamp(int ar, int man, int dagur, int klst, int min) {
+    	//Hér þarf að breyta í timestamp.
+    	final Calendar c = Calendar.getInstance();
+    	c.set(ar,man,dagur,klst,min);
+    	return 0;
     }
 }
 
--- timeandplace/src/is/hi/lucky7/timeandplace/viewEvent.java
+++ timeandplace/src/is/hi/lucky7/timeandplace/viewEvent.java
@@ -2,31 +2,22 @@ package is.hi.lucky7.timeandplace;
 
 import android.app.ListActivity;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.view.View;
+import android.content.Intent;
 import android.database.Cursor;
+import android.view.View;
+import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 public class viewEvent extends ListActivity{
 		private DBAdapter dba;
-		private Cursor cur;
 		
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.viewevent);
+		//setContentView(R.layout.viewevent);
 		dba = new DBAdapter(this);
 		dba.open();
 		fillData();
-		
-		final TextView view = (TextView) findViewById(R.id.view);
-
-		view.append(Integer.toString(dba.getEventCount()));
-		
-		final EditText edit_ViewNum = (EditText) findViewById(R.id.edit_viewNum);
-		final Button viewEventButton = (Button) findViewById(R.id.viewEventButton);
-
 	}
 		private void fillData() {
 	        // Get all of the notes from the database and create the item list
@@ -41,6 +32,18 @@ public class viewEvent extends ListActivity{
 	            new SimpleCursorAdapter(this, R.layout.event_row, c, from, to);
 	        setListAdapter(ev);
 		}
+	    protected void onListItemClick(ListView l, View v, int position, long id) {
+	    	super.onListItemClick(l, v, position, id);
+	    	//Ná í það sem ýtt var á.
+	    	Object o = this.getListAdapter().getItemId(position);
+	    	String keyword = o.toString();
+	    	//Toast.makeText(this, "Þú valdir: " + keyword, Toast.LENGTH_LONG).show();
+			Intent viewOneEvent = new Intent(getApplicationContext(), 
+					viewOneEvent.class);
+					viewOneEvent.putExtra("id",keyword);
+			startActivity(viewOneEvent);
+	    }
 	
 }
  
+//Prufa prufa
